efec990f8fdf5cd6d19b26b56ad0fc827ce68e49
mitsui3point <rladbsgh0331@gmail.com>
2022-10-13 15:14:30 +0900

    feat(CarTest, Car): add
    add CarTest
    - methods(회차_객체_세팅, 회차_객체_세팅_호출_예외처리, 자동차_이름_유효성_체크, 자동차_이름_유효성_체크_예외처리)
    
    add Car
    - Constructor
    - methods(setMembers, checkMembers, isInvalidName, addRound, checkRound, isInvalidRound, getRound, checkRoundIndex, isInvalidRoundIndex)

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,65 @@
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Car {
+    public static final int MIN_ROUND_INDEX = 0;
+    public static final int MIN_NAME_LENGTH = 1;
+    public static final int MAX_NAME_LENGTH = 5;
+    private String name;
+    private List<Round> rounds;
+
+    public Car(String name) {
+        this.checkMembers(name);
+        this.setMembers(name);
+    }
+
+    private void setMembers(String name) {
+        this.name = name;
+        this.rounds = new ArrayList<>();
+    }
+
+    private void checkMembers(String name) {
+        if(isInvalidName(name)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean isInvalidName(String name) {
+        return name == null ||
+                name.length() < MIN_NAME_LENGTH ||
+                name.length() > MAX_NAME_LENGTH;
+    }
+
+    public void addRound(Round round) {
+        this.checkRound(round);
+        this.rounds.add(round);
+    }
+
+    private void checkRound(Round round) {
+        if(isInvalidRound(round)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean isInvalidRound(Round round) {
+        return round == null;
+    }
+
+    public Round getRound(int roundIndex) {
+        this.checkRoundIndex(roundIndex);
+        return this.rounds.get(roundIndex);
+    }
+
+    private void checkRoundIndex(int roundIndex) {
+        if(isInvalidRoundIndex(roundIndex)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean isInvalidRoundIndex(int roundIndex) {
+        return roundIndex < MIN_ROUND_INDEX ||
+                roundIndex > this.rounds.size();
+    }
+}
---
+++ src/test/java/racingcar/CarTest.java
@@ -0,0 +1,81 @@
+package racingcar;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.constant.RoundStatus;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class CarTest {
+
+    private Car car;
+
+    @BeforeEach
+    void setUp() {
+        this.car = new Car("test1");
+        this.car.addRound(new Round(3));
+        this.car.addRound(new Round(4));
+        this.car.addRound(new Round(5));
+    }
+
+    @Test
+    void 회차_객체_세팅() {
+        RoundStatus actual1 = this.car.getRound(0).getRoundStatus();
+        RoundStatus actual2 = this.car.getRound(1).getRoundStatus();
+        RoundStatus actual3 = this.car.getRound(2).getRoundStatus();
+        RoundStatus expected1 = RoundStatus.STOP;
+        RoundStatus expected2 = RoundStatus.FORWARD;
+        RoundStatus expected3 = RoundStatus.FORWARD;
+        assertThat(actual1).isEqualTo(expected1);
+        assertThat(actual2).isEqualTo(expected2);
+        assertThat(actual3).isEqualTo(expected3);
+    }
+
+    @Test
+    void 회차_객체_세팅_예외처리() {
+        this.car = new Car("test2");
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> {
+            this.car.addRound(null);
+        });
+    }
+
+    @Test
+    void 회차_객체_세팅_호출_예외처리() {
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> {
+            this.car.getRound(-1).getRoundStatus();
+        });
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> {
+            this.car.getRound(-2).getRoundStatus();
+        });
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> {
+            this.car.getRound(4).getRoundStatus();
+        });
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "kim", "park", "jessi",
+            "KIM", "PARK", "JESSI",
+            "321", "9876", "12345",
+            "!@#$%", "^&*()", "\"[]{}", ";':|", ",.<>/", "?_+-="})
+    void 자동차_이름_유효성_체크(String name) {
+        assertThatNoException()
+                .isThrownBy(() -> {
+                    this.car = new Car(name);
+                });
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {
+            "abcdef", "123456", "12 456"})
+    void 자동차_이름_유효성_체크_예외처리(String name) {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> {
+                    this.car = new Car(name);
+                });
+    }
+}
